3158718a31e763ebbf9fb3e2c607f86d2555925e
mill1 <enijhuis@outlook.com>
2025-12-06 15:35:44 +0100

    Solved Day 6 Part 2

--- AdventOfCode2025/Puzzles/Puzzle6.cs
+++ AdventOfCode2025/Puzzles/Puzzle6.cs
@@ -1,5 +1,4 @@
 ﻿using AdventOfCode2025.Puzzles.Interfaces;
-using System.Text;
 
 namespace AdventOfCode2025.Puzzles
 {
@@ -8,10 +7,7 @@ namespace AdventOfCode2025.Puzzles
         public void Part1(bool useExample)
         {
             var lines = useExample ? GetExampleData() : File.ReadAllLines(@"Puzzles\Input\InputPuzzle6.txt").ToList();
-            var problems = new List<List<string>>();
-
-            for (int i = 0; i < lines.Count; i++)
-                problems.Add(SingularizeSpaces(lines[i].Trim()).Split(' ').ToList());
+            List<List<string>> problems = GetProblems(lines);
 
             long grandTotal = 0;
 
@@ -48,7 +44,116 @@ namespace AdventOfCode2025.Puzzles
 
         public void Part2(bool useExample)
         {
-            throw new NotImplementedException();
+            var lines = useExample ? GetExampleData() : File.ReadAllLines(@"Puzzles\Input\InputPuzzle6.txt").ToList();
+            var problemData = lines.Take(lines.Count - 1).ToList();
+            var problems = ParseColumnProblems(problemData);
+
+            var operations = SingularizeSpaces(lines.Last().Trim()).Split(' ').ToList();
+
+            long grandTotal = 0;
+
+            for (int i = 0; i < problems.Count; i++)
+            {
+                long total = 0;
+
+                for (int j = 0; j < problems[i].Count; j++)
+                {
+                    var value = problems[i][j];
+
+                    switch (operations[i])
+                    {
+                        case "+":
+                            total += value;
+                            break;
+
+                        case "*":
+                            if (j == 0)
+                                total = value;
+                            else
+                                total *= value;
+                            break;
+
+                        default:
+                            throw new NotImplementedException();
+                    }
+                }
+                grandTotal += total;
+            }
+
+            Console.WriteLine($"Grand total: {grandTotal}");
+        }            
+
+        public static List<List<long>> ParseColumnProblems(List<string> lines)
+        {
+            int rows = lines.Count;
+            int cols = lines[0].Length;
+            var results = new List<List<long>>();
+            int col = 0;
+
+            while (col < cols)
+            {
+                // Skip problem separators
+                bool isSeparator = true;
+                for (int r = 0; r < rows; r++)
+                {
+                    if (lines[r][col] != ' ')
+                    {
+                        isSeparator = false;
+                        break;
+                    }
+                }
+
+                if (isSeparator)
+                {
+                    col++;
+                    continue;
+                }
+
+                // This is a problem column; collect consecutive non-separator columns
+                var problemCols = new List<int>();
+                while (col < cols)
+                {
+                    bool empty = true;
+                    for (int r = 0; r < rows; r++)
+                    {
+                        if (lines[r][col] != ' ')
+                        {
+                            empty = false;
+                            break;
+                        }
+                    }
+                    if (empty) 
+                        break;
+
+                    problemCols.Add(col);
+                    col++;
+                }
+
+                // Parse each number vertically
+                var numbers = new List<long>();
+                foreach (int c in problemCols)
+                {
+                    string num = "";
+                    for (int r = 0; r < rows; r++)
+                    {
+                        num += lines[r][c];
+                    }
+                    numbers.Add(long.Parse(num));
+                }
+                results.Add(numbers);
+            }
+
+            return results;
+        }
+
+        private List<List<string>> GetProblems(List<string> lines)
+        {
+            var problems = new List<List<string>>();
+
+            for (int i = 0; i < lines.Count; i++)
+                problems.Add(SingularizeSpaces(lines[i].Trim()).Split(' ').ToList());
+
+            return problems;
         }
 
         private string SingularizeSpaces(string line)
@@ -65,7 +170,7 @@ namespace AdventOfCode2025.Puzzles
             {
                 "123 328  51 64 ",
                 " 45 64  387 23 ",
-                " 6 98  215 314",
+                "  6 98  215 314",
                 "*   +   *   +  "
             };
         }
