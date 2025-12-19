42e100844c49dc46baed3d51e8267a2f6b26a76e
mill1 <enijhuis@outlook.com>
2025-12-19 16:59:21 +0100

    Solved day 7

--- AdventOfCode2025/Puzzles/Puzzle7.cs
+++ AdventOfCode2025/Puzzles/Puzzle7.cs
@@ -4,64 +4,53 @@ namespace AdventOfCode2025.Puzzles
 {
     public class Puzzle7 : IPuzzle
     {
-        private List<char[]> _grid;
-        private int _total;
 
         public void Part1(bool useExample)
         {
-            _grid = useExample 
-                ? GetExampleData().Select(line => line.ToCharArray()).ToList() 
-                : File.ReadAllLines(this.GetPathInputFile()).Select(line => line.ToCharArray()).ToList();
-
-            Console.WriteLine(this.ToString());
-
-            // Find starting position
-            int startIndex = Array.IndexOf(_grid[0], 'S');
-            // Initialize second line
-            _grid[1][startIndex] = '|';
-
-            Console.WriteLine(this.ToString());
-
-            for (int row = 2; row < _grid.Count; row++)
-            {
-                ProcessBeams(row);
-
-                if(useExample)
-                    Console.WriteLine(this.ToString());
-            }
-
-            Console.WriteLine(this.ToString());
+            Console.WriteLine($"Total: {Solve(useExample, true)}");
         }
-
-        private void ProcessBeams(int row)
-        {
-            for (int col = 0; col < _grid[row].Length ; col++)
-            {
-                if (_grid[row - 1][col] == '|')
-                {
-                    // Did we hit a splitter?
-                    if (_grid[row][col] == '^')
-                    {
-                        _total++;
-
-                        // Split beam.
-                        if (col > 0)
-                            _grid[row][col - 1] = '|';
-
-                        if (col < _grid[row].Length - 1)
-                            _grid[row][col + 1] = '|';
-                    }
-                    else 
-                    {
-                        _grid[row][col] = '|';
-                    }
-                }
-            }            
-        }
-
         public void Part2(bool useExample)
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"Total: {Solve(useExample, false)}");
+        }
+
+        private long Solve(bool useExample, bool part1)
+        {
+            long totalPart1 = 0L;
+            long totalPart2 = 0L;
+
+            var grid = useExample
+                            ? GetExampleData().Select(line => line.ToCharArray()).ToList()
+                            : File.ReadAllLines(this.GetPathInputFile()).Select(line => line.ToCharArray()).ToList();
+
+            var rowCounts = new long[grid.Count];
+
+            // Find starting position
+            int startIndex = Array.IndexOf(grid[0], 'S');
+
+            rowCounts[startIndex] = 1;
+
+            for (var row = 1; row < grid.Count; row++)
+            {
+                for (var col = 0; col < grid[0].Length; col++)
+                {
+                    if (grid[row][col] == '^')
+                    {
+                        var count = rowCounts[col];
+                        rowCounts[col - 1] += count;
+                        rowCounts[col] = 0;
+                        rowCounts[col + 1] += count;
+
+                        if (count > 0)
+                            totalPart1++;
+                    }
+                }
+            }
+
+            foreach (var count in rowCounts)
+                totalPart2 += count;
+
+            return part1 ? totalPart1 : totalPart2;
         }
 
         private static List<string> GetExampleData()
@@ -85,15 +74,6 @@ namespace AdventOfCode2025.Puzzles
                 ".^.^.^.^.^...^.",
                 "..............."
             };
-        }
-
-        public override string? ToString()
-        {
-            // Convert List<char[]> to List<string>:
-            var lines = _grid.Select(arr => new string(arr)).ToList();
-            var matrix = string.Join(Environment.NewLine, lines);
-
-            return $"{matrix}{Environment.NewLine}Total: {_total}{Environment.NewLine}";
         }
     }
 }
