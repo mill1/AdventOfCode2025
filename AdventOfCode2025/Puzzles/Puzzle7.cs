4c1ddb90ad7e283f28ced8a48536a47cfeb6bd38
mill1 <enijhuis@outlook.com>
2025-12-19 22:09:04 +0100

    refactor puzzle 7

--- AdventOfCode2025/Puzzles/Puzzle7.cs
+++ AdventOfCode2025/Puzzles/Puzzle7.cs
@@ -4,23 +4,19 @@ namespace AdventOfCode2025.Puzzles
 {
     public class Puzzle7 : IPuzzle
     {
-
         public void Part1(bool useExample)
-        {
-            Console.WriteLine($"Total: {Solve(useExample).part1}");
-        }
+            => Console.WriteLine($"Total: {Solve(useExample).part1}");
+
         public void Part2(bool useExample)
-        {
-            Console.WriteLine($"Total: {Solve(useExample).part2}");
-        }
+            => Console.WriteLine($"Total: {Solve(useExample).part2}");
 
         private (long part1, long part2) Solve(bool useExample)
         {
             var grid = LoadGrid(useExample);
-            var state = InitializeState(grid);
+            var rowCounts = InitializeRowCounts(grid);
 
-            long part1 = Simulate(grid, state);
-            long part2 = state.RowCounts.Sum();
+            long part1 = Simulate(grid, rowCounts);
+            long part2 = rowCounts.Sum();
 
             return (part1, part2);
         }
@@ -34,32 +30,31 @@ namespace AdventOfCode2025.Puzzles
             return lines.Select(l => l.ToCharArray()).ToList();
         }
 
-        private SimulationState InitializeState(List<char[]> grid)
+        private long[] InitializeRowCounts(List<char[]> grid)
         {
             int startCol = Array.IndexOf(grid[0], 'S');
 
-            var state = new SimulationState(grid[0].Length);
-            state.RowCounts[startCol] = 1;
+            var rowCounts = new long[grid[0].Length];
+            rowCounts[startCol] = 1;
 
-            return state;
+            return rowCounts;
         }
 
-        private long Simulate(List<char[]> grid, SimulationState state)
+        private long Simulate(List<char[]> grid, long[] rowCounts)
         {
-            long usedSplitters = 0;
+            long splitCount = 0;
 
             for (int row = 1; row < grid.Count; row++)
             {
                 for (int col = 0; col < grid[row].Length; col++)
                 {
                     if (grid[row][col] == '^')
-                        usedSplitters += SplitAt(state.RowCounts, col);
+                        splitCount += SplitAt(rowCounts, col);
                 }
             }
 
-            return usedSplitters;
+            return splitCount;
         }
-
 
         private long SplitAt(long[] counts, int col)
         {
@@ -77,34 +72,25 @@ namespace AdventOfCode2025.Puzzles
         private static List<string> GetExampleData()
         {
             return new List<string>
-            {
-                ".......S.......",
-                "...............",
-                ".......^.......",
-                "...............",
-                "......^.^......",
-                "...............",
-                ".....^.^.^.....",
-                "...............",
-                "....^.^...^....",
-                "...............",
-                "...^.^...^.^...",
-                "...............",
-                "..^...^.....^..",
-                "...............",
-                ".^.^.^.^.^...^.",
-                "..............."
-            };
-        }
-
-        private class SimulationState
         {
-            public long[] RowCounts { get; }
-
-            public SimulationState(int width)
-            {
-                RowCounts = new long[width];
-            }
+            ".......S.......",
+            "...............",
+            ".......^.......",
+            "...............",
+            "......^.^......",
+            "...............",
+            ".....^.^.^.....",
+            "...............",
+            "....^.^...^....",
+            "...............",
+            "...^.^...^.^...",
+            "...............",
+            "..^...^.....^..",
+            "...............",
+            ".^.^.^.^.^...^.",
+            "..............."
+        };
         }
     }
+
 }
