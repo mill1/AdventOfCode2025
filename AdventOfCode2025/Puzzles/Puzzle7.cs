d8062214cc6133a5c9d10009437fa05b5806f154
mill1 <enijhuis@outlook.com>
2025-12-19 12:56:52 +0100

    Solved day 7 part 1

--- AdventOfCode2025/Puzzles/Puzzle7.cs
+++ AdventOfCode2025/Puzzles/Puzzle7.cs
@@ -4,13 +4,59 @@ namespace AdventOfCode2025.Puzzles
 {
     public class Puzzle7 : IPuzzle
     {
+        private List<char[]> _grid;
+        private int _total;
+
         public void Part1(bool useExample)
         {
-            var lines = useExample ? GetExampleData() : File.ReadAllLines(this.GetPathInputFile()).ToList();
+            _grid = useExample 
+                ? GetExampleData().Select(line => line.ToCharArray()).ToList() 
+                : File.ReadAllLines(this.GetPathInputFile()).Select(line => line.ToCharArray()).ToList();
 
-            var solution = 0;
+            Console.WriteLine(this.ToString());
 
-            Console.WriteLine($"Solution: {lines.Count}");
+            // Find starting position
+            int startIndex = Array.IndexOf(_grid[0], 'S');
+            // Initialize second line
+            _grid[1][startIndex] = '|';
+
+            Console.WriteLine(this.ToString());
+
+            for (int row = 2; row < _grid.Count; row++)
+            {
+                ProcessBeams(row);
+
+                if(useExample)
+                    Console.WriteLine(this.ToString());
+            }
+
+            Console.WriteLine(this.ToString());
+        }
+
+        private void ProcessBeams(int row)
+        {
+            for (int col = 0; col < _grid[row].Length ; col++)
+            {
+                if (_grid[row - 1][col] == '|')
+                {
+                    // Did we hit a splitter?
+                    if (_grid[row][col] == '^')
+                    {
+                        _total++;
+
+                        // Split beam.
+                        if (col > 0)
+                            _grid[row][col - 1] = '|';
+
+                        if (col < _grid[row].Length - 1)
+                            _grid[row][col + 1] = '|';
+                    }
+                    else 
+                    {
+                        _grid[row][col] = '|';
+                    }
+                }
+            }            
         }
 
         public void Part2(bool useExample)
@@ -39,6 +85,15 @@ namespace AdventOfCode2025.Puzzles
                 ".^.^.^.^.^...^.",
                 "..............."
             };
+        }
+
+        public override string? ToString()
+        {
+            // Convert List<char[]> to List<string>:
+            var lines = _grid.Select(arr => new string(arr)).ToList();
+            var matrix = string.Join(Environment.NewLine, lines);
+
+            return $"{matrix}{Environment.NewLine}Total: {_total}{Environment.NewLine}";
         }
     }
 }
